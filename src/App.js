ea5150807491594c607df1a8ee3df3dc9913b06a
leacau <97464095+leacau@users.noreply.github.com>
2022-12-26 08:58:22 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <BrowserRouter>
       <AuthProvider>
         <Routes>
-          <Route path="/" element={<Register /} />
+          <Route path="/" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
